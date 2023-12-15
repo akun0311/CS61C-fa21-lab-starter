1a2b9d5e77cc76d8407fbf6c9cefd19b6ad4d08f
akun <2969842877@qq.com>
2023-12-15 23:24:16 +0800

    finish lab01

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -1,6 +1,17 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
+// return 1 if the linkedlist has circle
+// return 0 if don't
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    node *fastPtr = head;
+    node *lastPtr = head;
+    while(fastPtr != NULL && fastPtr->next != NULL){
+        fastPtr = fastPtr->next->next;
+        lastPtr = lastPtr->next;
+        if(fastPtr == lastPtr){
+            return 1;
+        }
+    }
+    return 0;
 }
