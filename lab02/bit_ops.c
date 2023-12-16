a11d089fc5946fd16a737aac56578b5bcd6f97a1
akun <2969842877@qq.com>
2023-12-16 17:34:21 +0800

    try to implement set_bit

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -3,13 +3,31 @@
 
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
 unsigned get_bit(unsigned x, unsigned n) {
-    /* YOUR CODE HERE */
-    return -1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
+    // 1    2    3    4    5    6    7    8
+    // xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx 
+    // 0100 1110
+    //        10      
+    unsigned  num = 1 << n;
+    return (num & x) >> n;
 }
 
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
-void set_bit(unsigned *x, unsigned n, unsigned v) {
-    /* YOUR CODE HERE */
+void set_bit(unsigned *x, unsigned n, unsigned v) {    
+    unsigned num = 1 << n;
+    if(v == 1){
+
+    }else if (v == 0){
+        (*x) & (~num);
+    }
+    //old: 0100 1110
+//               ^
+//    tmp: 1111 1011
+/   //exp: 0100 1010   
+
+    //new: 0100 1110     
+  
+  
+
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
